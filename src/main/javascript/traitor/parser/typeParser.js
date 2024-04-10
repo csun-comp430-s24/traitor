d94bba597b102c371af77bd31c218188c031ca55
jreyes <justin.reyes.145@my.csun.edu>
2024-04-10 15:55:45 -0700

    finished type and comma_type parsing

--- src/main/javascript/traitor/parser/typeParser.js
+++ src/main/javascript/traitor/parser/typeParser.js
@@ -19,7 +19,10 @@ const parseBuiltIn = (tokenList, tokenPos) => {
     else if (token.type == 'variable') {
         return [{type:'StructType', name:token.data}, tokenPos+1];
     }
-    else return [null, tokenPos];
+    else if (token.type == 'lParen' || token.type == 'rParen' || token.type == 'rightArrow' || token.type == 'comma') {
+        return [null, tokenPos];
+    }
+    else throw Error("Parse Error Unknown Type: " + token.data);
 }
 
 const parseParenType = (tokenList, tokenPos) => {
@@ -32,31 +35,47 @@ const parseParenType = (tokenList, tokenPos) => {
             if (token.type == 'rParen') {
                 return [{type:'ParenType', value:parseResult}, tokenPos+1];
             }
-            else throw Error('Parse Error');
+            else throw Error('Parse Error No Right Paren On ParenType');
         }
         else return [null, tokenPos-1];
     }
     else return [null, tokenPos];
 }
 
-const parseFunctionType = (tokenList, tokenPos) => {
+const parseCommaType = (tokenList, tokenPos) => {
+    const resultList = [];
     var parseResult;
+    [parseResult, tokenPos] = parseType(tokenList, tokenPos);
+    while (parseResult != null) {
+        resultList.push(parseResult);
+        if (tokenList[tokenPos].type == 'comma') {
+            tokenPos++;
+        }
+        [parseResult, tokenPos] = parseType(tokenList, tokenPos);
+    }
+    return [{type:'CommaType', list:resultList}, tokenPos];
+}
+
+const parseFunctionType = (tokenList, tokenPos) => {
+    var inParseResult, outParseResult;
     var token = tokenList[tokenPos];
-    if (token.type == 'lParen') {
+    if (tokenPos < tokenList.length && token.type == 'lParen') {
         tokenPos++;
+        [inParseResult, tokenPos] = parseCommaType(tokenList, tokenPos);
         token = tokenList[tokenPos];
-        if (token.type == 'rParen') {
+        if (tokenPos < tokenList.length && token.type == 'rParen') {
             tokenPos++;
             token = tokenList[tokenPos];
-            if (token.type == 'rightArrow') {
-                [parseResult, tokenPos] = parseType(tokenList, tokenPos+1);
-                if (parseResult != null) {
-                    return [{type:'FuncType', out:parseResult}, tokenPos];
+            if (tokenPos < tokenList.length && token.type == 'rightArrow') {
+                [outParseResult, tokenPos] = parseType(tokenList, tokenPos+1);
+                if (outParseResult != null) {
+                    return [{type:'FuncType', in:inParseResult, out:outParseResult}, tokenPos];
                 }
+                else throw Error('Parse Error No Out From FunctionType');
             }
-            else throw Error('Parse Error');
+            else return [null, tokenPos - 3];
         }
-        else throw Error('Parse Error');
+        else return [null, tokenPos - 2];
     }
     else return [null, tokenPos];
 }
@@ -70,19 +89,20 @@ const parseType = (tokenList, tokenPos) => {
     if (parseResult != null) {
         return [parseResult, tokenPos];
     }
-    [parseResult, tokenPos] = parseParenType(tokenList, tokenPos);
-    if (parseResult != null) {
-        return [parseResult, tokenPos];
-    }
     [parseResult, tokenPos] = parseFunctionType(tokenList, tokenPos);
     if (parseResult != null) {
         return [parseResult, tokenPos];
     }
+    [parseResult, tokenPos] = parseParenType(tokenList, tokenPos);
+    if (parseResult != null) {
+        return [parseResult, tokenPos];
+    }
+    
     else return [null, tokenPos];
 }
 
 // TESTING CODE
-const test = "IntWrapper Void Boolean Self (Int) () => () => (Int)";
+const test = "(IntWrapper, Void, Boolean) => () => (Int) => (Boolean) => (Self)";
 const tokens = main(test);
 var parseResult;
 var pos = 0;
